933cb84aa283c3a5850d026ead04e9e22183f37b
wtrawlings <rawlings.warren@gmail.com>
2020-01-20 10:54:23 -0500

    bind event handler review and arrow funct

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,34 +9,43 @@ class Counter extends Component {
         fontSize: 50,
         fontWeight: "bold"
     }
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>There are no tags</p>;
-        return <ul>{this.state.tags.map(tag => <li key={tag}> { tag }</li>)}</ul>;
+
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    handleIncrement = () => {
+        console.log("Increment clicked", this.state.count)
     }
+    // renderTags() {
+    //     if (this.state.tags.length === 0) return <p>There are no tags</p>;
+    //     return <ul>{this.state.tags.map(tag => <li key={tag}> { tag }</li>)}</ul>;
+    // }
     render() { 
         return (
             <div>
-                { this.state.tags.length === 0 && "Please create a new tag"}
-                {this.renderTags()};
-                {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span> 
-                <button className="btn btn-secondary btn-sm">Increment</button> */}
-                {/* <ul>
+                {/* { this.state.tags.length === 0 && "Please create a new tag"}
+                {this.renderTags()}; */}
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span> 
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button> 
+             <ul>
                     {this.state.tags.map(tag => <li key={tag}> { tag }</li>)}
-                </ul> */}
+                </ul> 
             </div>
         );
     }
 
-    // getBadgeClasses() {
-    //     let classes = "badge m-2 badge-";
-    //     classes += this.state.count === 0 ? "warning" : "primary";
-    //     return classes;
-    // }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
 
-    // formatCount() {
-    //     const {count} = this.state;
-    //     return count === 0 ? 'Zero': count;
-    // }
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? 'Zero': count;
+    }
 }
  
 export default Counter;
