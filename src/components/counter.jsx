002221fa00a701f3b38c032654dccecddbe37b6f
wtrawlings <rawlings.warren@gmail.com>
2020-01-20 16:16:47 -0500

    changed count to value

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: 0,
+        value: this.props.value,
         tags: []
     };
     styles = {
@@ -16,13 +16,16 @@ class Counter extends Component {
     // }
 
     handleIncrement = () => {
-        console.log("Increment clicked", this.state.count)
+        this.setState({ value: this.state.value+1 });
+        console.log("Increment clicked", this.state.value)
     }
+    
     // renderTags() {
     //     if (this.state.tags.length === 0) return <p>There are no tags</p>;
     //     return <ul>{this.state.tags.map(tag => <li key={tag}> { tag }</li>)}</ul>;
     // }
     render() { 
+        // console.log("props", this.props)
         return (
             <div>
                 {/* { this.state.tags.length === 0 && "Please create a new tag"}
@@ -38,13 +41,13 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const {count} = this.state;
-        return count === 0 ? 'Zero': count;
+        const {value} = this.state;
+        return value === 0 ? 'Zero': value;
     }
 }
  
